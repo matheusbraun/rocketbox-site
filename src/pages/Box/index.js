0b4b956c75a5f0840f12e42aee1204c4be7f2551
Matheus Braun <matheeusbl@gmail.com>
2019-04-19 19:32:18 -0300

    Site do app Rocketbox
    Site do app rocketbox

---
+++ src/pages/Box/index.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react';
+import api from './../../services/api';
+import { distanceInWords } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+import Dropzone from 'react-dropzone';
+import socket from 'socket.io-client';
+
+import { MdInsertDriveFile } from 'react-icons/md';
+
+import './styles.css';
+import logo from './../../assets/logo.svg';
+
+export default class Box extends Component {
+	state = {
+		box: {}
+	};
+
+	async componentDidMount () {
+		this.subscribeToNewFiles();
+
+		const box = this.props.match.params.id;
+		const response = await api.get(`boxes/${box}`);
+
+		this.setState({
+			box: response.data
+		});
+	}
+
+	handleUpload = (files) => {
+		files.forEach((file) => {
+			const data = new FormData();
+
+			data.append('file', file);
+
+			const box = this.props.match.params.id;
+			api.post(`boxes/${box}/files`, data);
+		});
+	};
+
+	subscribeToNewFiles = () => {
+		const box = this.props.match.params.id;
+		const io = socket('https://braunrocketbox-backend.herokuapp.com');
+
+		io.emit('connectRoom', box);
+
+		io.on('file', (data) => {
+			this.setState({
+				box: {
+					...this.state.box,
+					files: [
+						data,
+						...this.state.box.files
+					]
+				}
+			});
+		});
+	};
+
+	render () {
+		return (
+			<div id="box-container">
+				<header>
+					<img src={logo} alt="" />
+					<h1>{this.state.box.title}</h1>
+				</header>
+
+				<Dropzone onDropAccepted={this.handleUpload}>
+					{({ getRootProps, getInputProps }) => (
+						<div className="upload" {...getRootProps()}>
+							<input {...getInputProps()} />
+
+							<p>Arraste arquivos ou clique aqui</p>
+						</div>
+					)}
+				</Dropzone>
+
+				<ul>
+					{this.state.box.files &&
+						this.state.box.files.map((file) => (
+							<li key={file._id}>
+								<a className="fileInfo" href={file.url} target="_blank" rel="noopener noreferrer">
+									<MdInsertDriveFile size={24} color="#A5CFFF" />
+									<strong>{file.title}</strong>
+								</a>
+
+								<span>
+									Há {' '}
+									{distanceInWords(file.createdAt, new Date(), {
+										locale: pt
+									})}
+								</span>
+							</li>
+						))}
+				</ul>
+			</div>
+		);
+	}
+}
